74fbfc60a40b42f78c3d86f9bb3e8d3547d136eb
rpworksmods <kyej1023@gmail.com>
2026-01-18 18:38:36 +0000

    Foundations Exercise 06
    - Succesfully passed all tests

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let result = []
+
+    if (num > 0) {
+        for (let i = 0; i < num; i++ ) {
+            // Get string and insert into result
+            result.push(string)
+        }
+    } else if (num == 0) {
+        return ""
+    } else {
+        return "ERROR"
+    }
+
+    return result.join("")
 };
 
 // Do not edit below this line
