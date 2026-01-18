787c501f99582d214a14cb6d0881a03400633db0
rpworksmods <kyej1023@gmail.com>
2026-01-18 20:33:11 +0000

    Foundations Exercise 09
    - Created a function to sum all numbers between min and max parameters
    - Ensured function doesn't work with negative numbers or non-integers
    - Ensured function reverses min and max values if necessary (i.e. user inputs min as higher than max)

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // Check if full integer and whether is a positive number
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
 
+    // Get whether a or b is bigger, and reverse if needed
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    
+    // Iterate through each number between min and max and add to the sum value
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
