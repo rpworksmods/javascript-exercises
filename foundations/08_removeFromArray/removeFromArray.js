29ce1c9686e23bdea05b70b08e2b7973ab9ce96a
rpworksmods <kyej1023@gmail.com>
2026-01-18 20:27:36 +0000

    Foundations Exercise 08
    - Wrote a function to remove any values in an array matching the given arguments

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // Filter through array, and check if each item is not included in args
+    return array.filter(x => !args.includes(x))
 };
 
 // Do not edit below this line
