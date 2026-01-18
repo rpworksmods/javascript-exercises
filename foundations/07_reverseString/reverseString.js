a1971da6d8cd80008fe484a090cf14fa7ab4645a
rpworksmods <kyej1023@gmail.com>
2026-01-18 20:21:15 +0000

    Foundations Exercise 07
    - Succesfully wrote a function to reverse a string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Make string into array (seperate letters with splice or whatever)
+    let array = string.split('');
 
+    // Use arr.sort to sort in opposite order
+    array = array.reverse();
+
+    // Return output as joined string
+    return array.join("");
 };
 
 // Do not edit below this line
