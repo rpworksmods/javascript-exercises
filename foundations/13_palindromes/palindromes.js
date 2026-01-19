90501502cec624388d652e4891faa7937a85d89f
rpworksmods <kyej1023@gmail.com>
2026-01-19 14:57:52 +0000

    Foundations Exercise 13
    - Wrote function that takes any size string and returns bool whether palindrome or not
    - Palindrome function works with strings including punctuation and whitespace

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    var newStr = string
+                .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                .toLowerCase()
+                .replace(/\s+/g, "");
+    var reverseString = newStr.split("").reverse().join("").toLowerCase();
+    
+    if (reverseString == newStr) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
