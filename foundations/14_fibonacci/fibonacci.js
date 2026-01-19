e2ca1f820d43b0d4c018c401877ece876772f08c
rpworksmods <kyej1023@gmail.com>
2026-01-19 15:28:16 +0000

    Foundations Exercise 14
    - Created function to get the relevant character of a given fibonacci sequence
    - Made function compatible with strings aswell as ints
    - Function escapes if provided a negative number

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // Convert num to integer if not already
+    num = parseInt(num, 10);
+    let initiator = [1, 1];
 
+    // Check if num is 0
+    if (num == 0) { return 0; }
+    if (num < 0) { return "OOPS"; }
+
+    // Make fibonacci number in array
+    for (let i = 0; i < num; i++) {
+        // Add the last two values of array together
+        let lastTwo = initiator.slice(Math.max(initiator.length - 2, 0))
+        let sum = lastTwo.reduce((a, b) => a + b,0)
+        
+        // Add new number to array
+        initiator.push(sum);
+    }
+
+    // Get the relevant result and return it
+    return initiator[num -1]
 };
 
 // Do not edit below this line
